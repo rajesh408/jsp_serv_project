5e06a236f02e9b8e5fdf09c9fa7b114fbc99db88
rajesh408 <52408258+rajesh408@users.noreply.github.com>
2023-07-25 18:06:24 +0530

    Add files via upload

---
+++ Role.java
@@ -0,0 +1,63 @@
+package com.rajesh.lms.model;
+
+import java.util.Date;
+
+import javax.swing.text.InternationalFormatter;
+
+public class Role {
+	private Integer id;
+	private String role;
+	private Date createDate;
+	private Role createdBy;
+	private Date updateDate;
+	private Role updatedBy;
+	private int status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Role getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Role createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public Role getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Role updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+	
+
+}
